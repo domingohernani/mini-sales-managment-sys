48168167492bfd9ca0009ae0e746530b9f5f3c7d
domingohernani <dev.hernani.domingo@gmail.com>
2026-02-02 16:32:21 +0800

    initial commit: project initialization

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({})
+
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+})
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
