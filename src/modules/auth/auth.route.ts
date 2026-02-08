3bb80fbbb002b01180c12d5ea4cdfb49853dc765
domingohernani <dev.hernani.domingo@gmail.com>
2026-02-08 16:54:29 +0800

    refactor: change auth route type error

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,9 @@
 import type { FastifyInstance } from "fastify";
 import { authenticate, refresh } from "./auth.controller.ts";
 
-const customerRoutes = async (fastify: FastifyInstance) => {
+const authRoutes = async (fastify: FastifyInstance) => {
   fastify.post("/authenticate", authenticate);
   fastify.post("/refresh", refresh);
 };
 
-export default customerRoutes;
+export default authRoutes;
